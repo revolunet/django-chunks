3708bf6207d97adfbc8fd854937c1038d59c5671
Julien Bouquillon <julien@bouquillon.com>
2010-04-15 01:31:37 +0200

    added tinyMce in admin

--- chunks/admin.py
+++ chunks/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from models import Chunk
 
 class ChunkAdmin(admin.ModelAdmin):
-  list_display = ('key',)
-  search_fields = ('key', 'content')
+    list_display = ('key',)
+    search_fields = ('key', 'content')
+    class Media:    # based on MEDIA_URL
+        js = ('js/tiny_mce/tiny_mce.js',
+              'js/tiny_config.js',)
 
 admin.site.register(Chunk, ChunkAdmin)
